99e28e89ea1e00cfdc61e29d34bfda2e326954ed
DaYeong Ji <dys621124@gmail.com>
2022-01-30 20:56:07 +0900

    오류 수정 + 카테고리 필터 추가

--- face/face_detect.py
+++ face/face_detect.py
@@ -15,7 +15,8 @@ def FaceDetect():
     img_person = cv2.inRange(img_hsv, lower, upper) #살색 범위를 출력
 
     #경계선 찾음
-    _, contours, hierarchy = cv2.findContours(img_person, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(img_person, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) # 아래 코드에서 오류 발생해서 수정함(다영)
+    # _, contours, hierarchy = cv2.findContours(img_person, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     # 가장 큰 영역 찾기
     max = 0
