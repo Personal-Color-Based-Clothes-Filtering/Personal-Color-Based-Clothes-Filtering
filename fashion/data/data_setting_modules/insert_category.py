793e20ede3a48f420ff4376797226e949fa3469f
Im Chaewon <lisa425@sookmyung.ac.kr>
2022-02-01 14:15:21 +0900

    tone 추가된 카테고리별 데이터셋 생성

--- fashion/data/data_setting_modules/insert_category.py
+++ fashion/data/data_setting_modules/insert_category.py
@@ -1,13 +1,17 @@
 #csv 데이터의 카테고리 칼럼을 추가하고, category 데이터를 집어넣는다. 
 import pandas as pd
 
-df = pd.read_csv('../predataset/sweat.csv')
-df['category'] = 'sweatshirt'
-df.head()
-df.to_csv('../dataset/sweatshirt.csv')
+# df = pd.read_csv('../predataset/sweat.csv')
+# df['category'] = 'sweatshirt'
+# df.head()
+# df.to_csv('../dataset/sweatshirt.csv')
 
 # df = pd.read_csv('../dataset/all.csv')
 # df['tone'] = 'spring'
 # df['color'] = 'red'
 # df.head()
 # df.to_csv('../dataset/all.csv')
+
+
+
+
