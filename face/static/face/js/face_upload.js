160fa913afc61e3f308f1f03f2961b803f575001
youjeongk <youjeongkang11@gmail.com>
2022-02-20 22:05:32 +0900

    로딩중ing

--- face/static/face/js/face_upload.js
+++ face/static/face/js/face_upload.js
@@ -44,11 +44,11 @@ function test(imageName) {
 }
  
 function LoadingWithMask(gif) {
-    //화면의 높이와 너비를 구합니다.
+    //화면의 높이와 너비 구하기.
     var maskHeight = $(document).height();
     var maskWidth  = window.document.body.clientWidth;
      
-    //화면에 출력할 마스크를 설정해줍니다.
+    //화면에 출력할 마스크 설정하기.
     var mask       ="<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
     var loadingImg ='';
       
@@ -58,7 +58,7 @@ function LoadingWithMask(gif) {
     $('body')
         .append(mask)
  
-    //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채웁니다.
+    //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채우기.
     $('#mask').css({
             'width' : maskWidth,
             'height': maskHeight,
